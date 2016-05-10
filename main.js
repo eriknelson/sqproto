405bf0f56cc3af3c428aec4c79d7b4f3c5bbbc5f
Erik Nelson <erik@nsk.io>
2016-05-10 14:40:24 -0400

    Dynamic data + grid layout example

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-var data = {
-  name: 'erik'
+var DATA = {
+  releases: 14,
+  last_update: '1 day ago', // Probably need to gen this string via moment.js or something
+  net_repos: 22,
+  forks: 10,
+  stars: 66,
+  branches: 5
 };
 
 $(function() {
+  // Build render func from template
   render = doT.template($('#report-widget-templ').text());
-  $('#report-widget-mount').html(render(data));
+
+  $('#load-data').click(function() {
+    //Simulate loading data, passing in cb
+    ajaxLoad(function (data) {
+      // Render Template after we loaded the data
+      $('#report-widget-mount').html(render(data));
+    });
+  });
+
 });
+
+function ajaxLoad(cb) {
+  // Inject data
+  var loadTime = 1000;
+  setTimeout(function() {
+    cb(DATA);
+  }, loadTime);
+}
